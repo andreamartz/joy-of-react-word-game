547478f445000736a30ab1180291d4afe8c53f77
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2024-02-04 10:27:37 -0600

    Improve the input form for guesses

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-import Guess from '../Guess/GuessInput';
+import GuessInput from '../GuessInput/GuessInput';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,7 +12,7 @@ console.info({ answer });
 function Game() {
   return (
     <>
-      <Guess />
+      <GuessInput />
     </>
   );
 }
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -4,31 +4,30 @@ const GuessInput = () => {
   const [guess, setGuess] = useState('');
 
   const handleChange = (event) => {
-    event.preventDefault();
-    const nextGuessValue = event.target.value.toUpperCase();
-    setGuess(nextGuessValue);
+    const nextGuess = event.target.value.toUpperCase();
+    console.log({ nextGuess });
+    setGuess(nextGuess);
+  };
 
-    if(nextGuessValue.length === 5) {
-      // submit the guess
-      handleSubmit(nextGuessValue);
-    }
-  }
-  
-  const handleSubmit = (guess) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
     console.log({ guess });
     setGuess('');
-  }
+  };
 
   return (
-    <form className="guess-input-wrapper" action="">
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
       <input id="guess-input" 
         type="text"
         required={true}
         onChange={handleChange} 
-        value={guess} 
-        title="Your guess must contain exactly five letters"
+        value={guess}
+        minLength={5}
+        maxLength={5}
+        title="5 letter word"
         pattern="([A-Z]{5})"
+        autoComplete="off"
       />
     </form>
   );
